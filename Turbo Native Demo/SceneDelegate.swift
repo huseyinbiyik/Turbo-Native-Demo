ed9deb51a6e86a48ed781771ce762659bf5c7bef
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-11-15 13:05:55 +0300

    Add turbo scaffold

--- Turbo Native Demo/SceneDelegate.swift
+++ Turbo Native Demo/SceneDelegate.swift
@@ -1,52 +1,52 @@
-//
-//  SceneDelegate.swift
-//  Turbo Native Demo
-//
-//  Created by Hüseyin Bıyık on 14.11.2023.
-//
-
 import UIKit
+import Turbo
+import WebKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    private let navigationController = UINavigationController()
+    private lazy var session: Session = {
+        let configuration = WKWebViewConfiguration()
+        configuration.applicationNameForUserAgent = "Turbo Native iOS"
+        
+        let session = Session(webViewConfiguration: configuration)
+        session.delegate = self
+        return session
+    }()
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+        
+        visit()
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    private func visit() {
+        let url = URL(string: "http://localhost:3000")!
+        let controller = VisitableViewController(url: url)
+        session.visit(controller, action: .advance)
+        navigationController.pushViewController(controller, animated: true)
     }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
+extension SceneDelegate: SessionDelegate {
+    func session(_ session: Turbo.Session, didProposeVisit proposal: VisitProposal) {
+        let controller = VisitableViewController(url: proposal.url)
+        session.visit(controller, options: proposal.options)
+        navigationController.pushViewController(controller, animated: true)
+    }
+    
+    func session(_ session: Turbo.Session, didFailRequestForVisitable visitable: Turbo.Visitable, error: Error) {
+        //todo
+    }
+    
+    func sessionWebViewProcessDidTerminate(_ session: Turbo.Session) {
+        //todo
+    }
+    
+    
+}
